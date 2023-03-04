14ff0f1288b9738f3acdd4aff485a167a47c14c9
David Podeszwa <davi.podeszwa@gmail.com>
2023-03-05 00:15:43 +0100

    Show Key

--- src/Keys.hs
+++ src/Keys.hs
@@ -5,14 +5,17 @@ module Keys
 data PrivateKey = PrivateKey { 
     value :: Integer,
     theN :: Int
-}
+} deriving Show
 
 data PublicKey = PublicKey{
     x :: Integer,
     y :: Integer
-}
+} deriving Show
 
 data Key = Key{
     private :: PrivateKey,
     public :: PublicKey
 }
+
+instance Show Key where
+  show (Key private public) = "Key {\nd: "++show private ++ "\nQ: "++ show public ++ "\n}"
